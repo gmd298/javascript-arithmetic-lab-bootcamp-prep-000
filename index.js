d2e7fd1b819e495c282e5bb8864eec4d6de3b3fe
decisive-metric-0348 <gmd298@gmail.com>
2021-03-09 18:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,15 @@ function add(a, b) {
   return (a + b)
 }
 
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
